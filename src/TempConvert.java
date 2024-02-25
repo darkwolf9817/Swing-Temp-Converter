a1cda524fe8f9e16db78d16ca3c77809a16ba390
Tyler Kearney <75549435+darkwolf9817@users.noreply.github.com>
2024-02-25 15:30:58 -0600

    Code commit

---
+++ src/TempConvert.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class TempConvert {
+    private JComboBox cbxTempIn;
+    private JComboBox cbxTempOut;
+    private JTextField txtTempIn;
+    private JButton btnConvert;
+    private JLabel lblOut;
+}
